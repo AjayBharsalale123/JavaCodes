43c3ef9698b26d88f1c6f3ecfe14fdf2cda470ae
AjayBharsalale123 <60804925+AjayBharsalale123@users.noreply.github.com>
2022-04-04 14:38:29 +0530

    Add files via upload

---
+++ cw2feb.java
@@ -0,0 +1,23 @@
+
+public class cw2feb {
+
+	public static void main(String[] args) {
+		
+		
+		 double exp1 = (3.0 * 2 / 4);
+	        int exp2 = (3 * -2 % 4);
+
+	        System.out.println(exp1);
+	        System.out.println(exp2);
+
+//		int x=2,y=5;
+//		  int exp1 = (y * (x / y + x / y));
+//	        int exp2 = (y * x / y + y * x / y);
+//	        
+//	        System.out.println(exp1);
+//	        System.out.println(exp2);
+
+
+	}
+
+}
